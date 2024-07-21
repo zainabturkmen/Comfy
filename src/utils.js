44fa1d70942520147db254f02d92dcc89d32ac65
Zainab Turkmen <132147692+zainabturkmen@users.noreply.github.com>
2024-07-21 14:51:18 +0430

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -3,11 +3,11 @@
 //   DATA IS THE SAME JUST A DIFFERENT URL,
 //   DOES NOT AFFECT PROJECT FUNCTIONALITY
 
-const allProductsUrl = 'https://course-api.com/javascript-store-products'
+const allProductsUrl = 'https://www.course-api.com/javascript-store-products'
 // temporary single product
 // 'https://course-api.com/javascript-store-single-product?id=rec43w3ipXvP28vog'
 const singleProductUrl =
-  'https://course-api.com/javascript-store-single-product'
+  'https://www.course-api.com/javascript-store-single-product'
 
 const getElement = (selection) => {
   const element = document.querySelector(selection)
